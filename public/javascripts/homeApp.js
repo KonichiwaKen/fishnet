e75e2166fcf21cc9606b7057845ad440941dfb00
Inan Tainwala <inantainwala@gmail.com>
2015-04-24 22:59:15 -0400

    Changed date time format

--- public/javascripts/homeApp.js
+++ public/javascripts/homeApp.js
@@ -25,13 +25,13 @@ app.controller('HomeCtrl', [
       $scope.endTime = '';
       $scope.isPublic = false;
     }
-  }
-]);
+}]);
 
 app.factory('events', ['$http', '$window', function($http, $window) {
   var o = {
     events: []
   };
+
 
   o.addEvent = function(event) {
     $http.post('/events', event);
@@ -40,9 +40,49 @@ app.factory('events', ['$http', '$window', function($http, $window) {
   o.getEvents = function() {
     return $http.get('/events').success(function(data) {
       o.events.push.apply(o.events, angular.fromJson(data));
-      // console.log(o.events);
+      // loop = (o.events);
+      for(var i=0; i<o.events.length;i++){
+        o.events[i].startTime = convertDate(o.events[i].startTime);
+        o.events[i].endTime = convertDate(o.events[i].endTime);
+      }
     })
+  }
+
+
+  convertDate = function(dateString) {
+    var year = parseInt(dateString.substring(0, 4));
+    var month = parseInt(dateString.substring(5, 7));
+    var day = parseInt(dateString.substring(8, 10));
+    var hour = parseInt(dateString.substring(11, 13));
+    var minute = dateString.substring(14, 16);
+    var ampm;
+
+    if (hour >= 12) {
+      ampm = "PM";
+    } else {
+      ampm = "AM";
+    }
+
+    if (hour > 12) {
+      hour = hour - 12;
+    } else if (hour == 0) {
+      hour = 12;
+    }
+    var str = hour + ":" + minute + " " + ampm + " on " + month + "/" + day + "/" + year;
+    return str;
   }
 
   return o;
 }]);
+
+
+
+
+
+
+
+
+
+
+
+ 
